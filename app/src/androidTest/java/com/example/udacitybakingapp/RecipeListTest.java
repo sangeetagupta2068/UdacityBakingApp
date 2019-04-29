9d0c5cd9f45a9cf95d55b9ce6d09f74ba0e016a7
sangeetagupta <tinnigupta1998@gmail.com>
2019-04-29 07:47:21 +0530

    3 commit

--- app/src/androidTest/java/com/example/udacitybakingapp/RecipeListTest.java
+++ app/src/androidTest/java/com/example/udacitybakingapp/RecipeListTest.java
@@ -1,9 +1,13 @@
 package com.example.udacitybakingapp;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +21,23 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class RecipeListTest {
     @Rule
-    ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void clickOnRecyclerViewItem(){
+
+    }
+
+    private IdlingResource idlingResource;
+
+    @Before
+    public void registerIdlingResource(){
+        idlingResource = activityTestRule.getActivity().setIdlingResource();
+        Espresso.registerIdlingResources(idlingResource);
+    }
+
+    @Test
+    public void idlingResourceTest(){
         onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
 
         onView(withId(R.id.ingredient_label)).check(matches(isDisplayed()));
@@ -30,5 +47,12 @@ public class RecipeListTest {
 
         onView(withId(R.id.view_pager))
                 .check(matches(isDisplayed()));
+    }
+
+    @After
+    public void unregisterIdlingResource(){
+        if(idlingResource !=null){
+            Espresso.unregisterIdlingResources(idlingResource);
+        }
     }
 }
--- app/src/main/java/com/example/udacitybakingapp/MainActivity.java
+++ app/src/main/java/com/example/udacitybakingapp/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.udacitybakingapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.udacitybakingapp.IdlingResource.SimpleIdlingResource;
 import com.example.udacitybakingapp.fragments.RecipeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Nullable
+    private SimpleIdlingResource simpleIdlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    public SimpleIdlingResource setIdlingResource(){
+        if(simpleIdlingResource !=null){
+            return simpleIdlingResource;
+        } else {
+            simpleIdlingResource = new SimpleIdlingResource();
+            return simpleIdlingResource;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setIdlingResource();
 
         RecipeFragment rf = new RecipeFragment();
         FragmentManager fm = getSupportFragmentManager();
